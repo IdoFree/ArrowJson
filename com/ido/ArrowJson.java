923ae4e17b37de939705e1a029dbd335583d53aa
IdoFree <306634799@qq.com>
2017-05-09 15:06:45 +0800

    add code to support Json Stream to Object

--- com/ido/ArrowJson.java
+++ com/ido/ArrowJson.java
@@ -1,4 +1,4 @@
-package hss.isis.rds.dao.jdbc;
+package hss.isis.gtap.vbs.utils;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -7,12 +7,12 @@ import java.util.List;
 import org.junit.Test;
 
 public class ArrowJson {
-	private static String ARRAY_START = "[ ";
-	private static String ARRAY_END = " ]";
-	private static String OBJECT_END = " }";
-	private static String OBJECT_START = "{ ";
-	private static String COMMA = ", ";
-	private static String COLON = " : ";
+	private static String ARRAY_START = "[";
+	private static String ARRAY_END = "]";
+	private static String OBJECT_END = "}";
+	private static String OBJECT_START = "{";
+	private static String COMMA = ",";
+	private static String COLON = ":";
 	
 	static public String toJson(Object obj) throws IllegalArgumentException,
 			IllegalAccessException {
@@ -53,11 +53,14 @@ public class ArrowJson {
 		for (Field f : allFileds) {
 			f.setAccessible(true);
 			Object val = f.get(obj);
+			if(val == null){
+				continue;
+			}
+			
 			if (val instanceof String) {
 				appendQuotation(appendQuotation(sb, f.getName()).append(COLON),
 						(String) val).append(COMMA);
-			} else if (val instanceof Integer || val instanceof Float
-					|| val instanceof Double || val instanceof Number) {
+			} else if ( val instanceof Number) {
 				appendQuotation(sb, f.getName()).append(COLON).append(val)
 						.append(COMMA);
 			}else {
@@ -89,17 +92,81 @@ public class ArrowJson {
 		in.append("\"").append(name).append("\"");
 		return in;
 	}
+	
+	/**
+	 * 
+	 * @param clz the target object type to create
+	 * @param jsonString the json string 
+	 * @return the initialized target object
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 */
+	public static <T> T jsonToObject(Class<T> clz,String jsonString) throws InstantiationException, IllegalAccessException{
+		T result = clz.newInstance();
+		for(;;){
+			Token token = Lexemer.getToken(jsonString);
+			if(token.type == TokenType.ID ){
+				return null;
+			}
+		}
+	}
+	
+	private static class Parser{
+		
+	}
+	
+	private static class Lexemer{
+		private StringBuffer sb = new StringBuffer();
+		private static final byte BRACE_START = '{';
+		private static int temp = 0;
+		
+		public static Token getToken(String jsonStream){
+			if(jsonStream == null || jsonStream == ""){
+				return null;
+			}
+			char[] ins = jsonStream.toCharArray();
+			for(int i = 0; i< ins.length ; i++){
+				
+				
+				char current = ins[i] ;
+				if(current == ' ' || current == '\t' || current == '\n' ){
+					continue;
+				}
+				
+				if(Character.isDigit(current)){
+					do{
+						temp = temp*10 + current;
+						current = ins[++i] ;
+					}while(Character.isDigit(current));
+				}
+				
+			}
+			
+			return null;
+		}
+	}
+	
+	
+	private static class Token{
+		 TokenType type;
+		 Object val;
+		
+	}
+	
+	private  enum TokenType{
+		Keyword,ID
+	}
 
 	private static class Person {
 		int age;
 		String name;
 	}
 
-	@Test
+//	@Test
 	public void testSingalObject() throws IllegalArgumentException, IllegalAccessException {
 		Person p = new Person();
 		p.age = 10;
-		p.name = "Ido";
+//		p.name = nu;
 
 		System.out.print(ArrowJson.toJson(p));
 	}
@@ -145,12 +212,12 @@ public class ArrowJson {
 		Address address;
 	}
 	
-//	@Test
+	@Test
 	public void testOjectNested() throws IllegalArgumentException, IllegalAccessException{
 		Department department = new Department();
 		department.person = new Person();
 		department.person.age = 10;
-		department.person.name = "ido";
+//		department.person.name = "ido";
 		department.name = "department name";
 		department.address = new Address();
 		department.address.homeNumber = 100;
